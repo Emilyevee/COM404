23a0a1ddf61d96d5867ffae449cc75dd1881bb9c
Emilyevee <4greee92@solent.ac.uk>
2019-09-25 09:54:21 +0000

    updated code

--- 0/main.py
+++ 0/main.py
@@ -1,1 +1,10 @@
 print("hello")
+print("how are ypu")
+name = "emily"
+height = 1.8
+initial = "e" 
+age = 18
+iscool = True 
+
+print ("hiya, " + name + " you are " + str(age))
+
---
+++ 1-basics/1-output/2-multiline-message/bot.py
@@ -0,0 +1,6 @@
+# Displat message to the standard output 
+print ("system failure imminent !")
+print ("system reboot has been initiated . . .")
+print ("...rebooting sensory system")
+print ("...rebooting output motors")
+print ("...rebooting hover engine")
