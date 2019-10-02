39951ae20e593d6b4067166f5e3434660b73dfa6
Emilyevee <4greee92@solent.ac.uk>
2019-10-02 11:15:45 +0000

    added code to demonstrate the use of a logical and operator

---
+++ 1-basics/3-decisions/10-and-operator/bot.py
@@ -0,0 +1,8 @@
+print ("what did i hear")
+noise = input ()
+print ("what did i see")
+see = input ()
+if ((noise == "grrrr") and (see == "two red eyes")):
+    print ("there is a scary creature. I should get out of here!")
+else:
+    print ("i am a little scared but i will continue")
